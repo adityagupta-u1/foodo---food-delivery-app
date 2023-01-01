b800d13fab0591671316eb2301208e644f085d2c
adityagupta-u1 <beenagupta934@gmail.com>
2023-01-01 18:55:17 +0530

    changes done

--- src/pages/orders/checkout.tsx
+++ src/pages/orders/checkout.tsx
@@ -68,7 +68,7 @@ const CheckOut = ({cart,razorpayKey,user,session}:{cart:Cart | null,razorpayKey:
                                     currency: order?.currency,
                                     amount: order?.amount,
                                     order_id: order?.id,
-                                    callback_url:`https://3067-103-224-54-79.in.ngrok.io/orders/${order.id} `,
+                                    callback_url:`https://foodo-food-delivery-app-ivio.vercel.app/orders/${order.id}`,
                                     description: "Thankyou for your test donation",
                                     notes:{
                                             address:values.address,
